7caab7afc49c2ba606bdb7a12085131cc33b99d7
toubaru <marlumarlumarlu@hotmail.com>
2018-08-27 16:09:36 +0900

    update

--- xss4.php
+++ xss4.php
@@ -21,21 +21,21 @@ $user = $users[$id];
   <div id="app">
     <h1>{{ title }}</h1>
     <div>
-      demoをclickするとXSSが発動します。
+      demoをclickするとXSSが発動します。<br>
       createElement でテンプレートを描画する際に、domProps で innerHTML にタグ文字を渡すと、v-htmlと同様にHTMLをそのまま出力できる。<br>
+      どうやったら自然に脆弱性が入るのか謎。<br>
       <a href="https://jp.vuejs.org/v2/guide/render-function.html#%E3%83%87%E3%83%BC%E3%82%BF%E3%82%AA%E3%83%96%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E8%A9%B3%E8%A7%A3" target="_blank">参考</a>
 
     </div>
-    <anchored-heading>Hello world!</anchored-heading>
+    <my-conponent>Hello world!</my-conponent>
     <p><a href="/xss4.php?id=2">demo</a></p>
   </div>
   <script>
-  window.addEventListener('load', function () {
     const props = JSON.parse('{"domProps": { "innerHTML": "<?=$user["user_name"] ?> さんのプロフィール" }}')
-    let anchoredHeading = Vue.component('anchored-heading', {
+    let myConponent = Vue.component('my-conponent', {
       render: function (createElement) {
         return createElement(
-          'h1', props, 'hoge'
+          'h1', props
         )
       },
     })
@@ -45,10 +45,9 @@ $user = $users[$id];
         title: 'Vue.js XSSサンプル 4'
       },
       components: {
-        'anchored-heading': anchoredHeading
+        'my-conponent': myConponent
       }
     })
-  })
   </script>
 </body>
 </html>
